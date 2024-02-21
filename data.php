379b08b5aec30186187c150a6c2df0e648cab08a
chababyassine <cyassinechabab@gmail.com>
2024-02-21 09:29:55 +0100

    first commit

---
+++ data.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+$_SESSION['motos']=array( 
+array("id"=>1,"model"=>"Suzuki","vendu"=>22,"stock"=>18, "image"=>"https://cdn.pixabay.com/photo/2014/07/31/23/10/biker-407123_1280.jpg"),
+array("id"=>2,"model"=>"Moto Guzzi V7 850","vendu"=>25,"stock"=>17,"image"=>"https://media.gqmagazine.fr/photos/5fe09dad5d3f3b4ecc545612/16:9/w_1920,c_limit/BMW-M-1000-RR.jpg"),
+array("id"=>3,"model"=>"Ducati Multistrada V4","vendu"=>23,"stock"=>11,"image"=>"https://media.gqmagazine.fr/photos/5fe0aa415d3f3b4ecc545625/master/w_1600,c_limit/BMW-Motorrad-M-100-RR-Motorcycle-9.jpg"),
+array("id"=>4,"model"=>"Yamaha MT07","vendu"=>22,"stock"=>14,"mage"=>"https://media.gqmagazine.fr/photos/5fe09f4f5d3f3b4ecc545615/master/w_1600,c_limit/triumph-scrambler-1200-bond-1.jpg"),
+array("id"=>5,"model"=>"","vendu"=>26,"stock"=>65,"image"=>""),
+);
+?>
---
+++ formulaire.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>formulaire de recupération</title>
+</head>
+<body>
+<form action="traitement.php" method="post"> 
+<div>
+    <label for="model">Model:</label>
+    <input type="text" id="model" name="model">
+</div>
+<div>
+    <label for="vendu">Vendu:</label>
+    <input type="text" id="vendu" name="vendu">
+</div>
+<div>
+    <label for="stock">stock:</label>
+    <input type="text" id="stock" name="stock">
+</div>
+</body>
+</html>
